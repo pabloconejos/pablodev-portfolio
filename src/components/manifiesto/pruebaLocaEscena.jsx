272a81ad658f5e54cbca7aaf1cdc777f8f0628a6
Pablo Conejos <pconejos03@gmail.com>
2025-11-17 20:02:11 +0100

    commit

---
+++ src/components/manifiesto/pruebaLocaEscena.jsx
@@ -0,0 +1,21 @@
+// Scene.jsx
+import { Canvas } from "@react-three/fiber";
+import { Environment } from "@react-three/drei";
+import PruebaLocaModel from "./pruebaLocaModel";
+import MarqueeStack from "./MarqueeStack";
+// "use client" si usas Next.js (app router)
+
+export default function Scene() {
+  return (
+    <Canvas style={{ backgroundColor: "black", position: "absolute", top: "83dvh"}}>
+      <directionalLight intensity={3} position={[0, 3, 2]} />
+      <Environment preset="studio" />
+
+      {/* Texto multi-fila */}
+      <MarqueeStack />
+
+      {/* Tu malla animada */}
+      <PruebaLocaModel />
+    </Canvas>
+  );
+}
